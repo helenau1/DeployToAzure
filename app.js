5f48149543dc21e3476c1332b3baffe125d54637
helenau1 <henzku@hotmail.com>
2018-01-25 10:07:17 +0200

    Update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
+var helena = require('./routes/helena');
 
 var app = express();
 
@@ -26,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/about', about);
+app.use('/helena', helena);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
